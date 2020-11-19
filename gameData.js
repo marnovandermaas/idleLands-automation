d0ace1ebfc5433840836a456279c354e0d653cd0
Ian Duchesne <ian.duchesne@gmail.com>
2020-11-19 09:56:20 -0500

    Create gameData.js

---
+++ gameData.js
@@ -0,0 +1,76 @@
+const rewards = {
+  'xp:player:sm': 'Player XP (Small)',
+  'xp:player:md': 'Player XP (Medium)',
+  'xp:player:lg': 'Player XP (Large)',
+  'xp:player:max': 'Player Level Up',
+  
+  'xp:pet:sm': 'Pet XP (Small)',
+  'xp:pet:md': 'Pet XP (Medium)',
+  'xp:pet:lg': 'Pet XP (Large)',
+  'xp:pet:max': 'Pet Level Up',
+
+  'gold:player:sm': 'Gold (Small)',
+  'gold:player:md': 'Gold (Medium)',
+  'gold:player:lg': 'Gold (Large)',
+  'gold:player:xl': 'Gold (XL)',
+
+  'ilp:player:sm': 'ILP (Small)',
+  'ilp:player:md': 'ILP (Medium)',
+  'ilp:player:lg': 'ILP (Large)',
+  
+  'resource:clay:sm': 'Clay (Small)',
+  'resource:clay:md': 'Clay (Medium)',
+  'resource:clay:lg': 'Clay (Large)',
+
+  'resource:wood:sm': 'Wood (Small)',
+  'resource:wood:md': 'Wood (Medium)',
+  'resource:wood:lg': 'Wood (Large)',
+  
+  'resource:stone:sm': 'Stone (Small)',
+  'resource:stone:md': 'Stone (Medium)',
+  'resource:stone:lg': 'Stone (Large)',
+
+  'resource:astralium:sm': 'Astralium (Small)',
+  'resource:astralium:md': 'Astralium (Medium)',
+  'resource:astralium:lg': 'Astralium (Large)',
+  
+  'collectible:Soul:Green': 'Pet Soul (Green)',
+  'collectible:Soul:Yellow': 'Pet Soul (Yellow)',
+  'collectible:Soul:Red': 'Pet Soul (Red)',
+  'collectible:Soul:Blue': 'Pet Soul (Blue)',
+  'collectible:Soul:Purple': 'Pet Soul (Purple)',
+  'collectible:Soul:Orange': 'Pet Soul (Orange)',
+
+  'item:Crystal:Green': 'Crystal (Green)',
+  'item:Crystal:Yellow': 'Crystal (Yellow)',
+  'item:Crystal:Red': 'Crystal (Red)',
+  'item:Crystal:Blue': 'Crystal (Blue)',
+  'item:Crystal:Purple': 'Crystal (Purple)',
+  'item:Crystal:Orange': 'Crystal (Orange)',
+  'item:Crystal:Astral': 'Crystal (Astral)',
+
+  'item:Crystal:Green:5': 'Crystal (Green x5)',
+  'item:Crystal:Yellow:5': 'Crystal (Yellow x5)',
+  'item:Crystal:Red:5': 'Crystal (Red x5)',
+  'item:Crystal:Blue:5': 'Crystal (Blue x5)',
+  'item:Crystal:Purple:5': 'Crystal (Purple x5)',
+  'item:Crystal:Orange:5': 'Crystal (Orange x5)',
+  'item:Crystal:Astral:5': 'Crystal (Astral x5)',
+
+  'item:generated:basic': 'Item (Basic)',
+  'item:generated:pro': 'Item (Pro)',
+  'item:generated:idle': 'Item (Idle)',
+  'item:generated:godly': 'Item (Godly)',
+  'item:generated:goatly': 'Item (Goatly)',
+  'item:generated:omega': 'Item (Omega)',
+  
+  'event:chosen:Enchant': 'Item Enchantment',
+
+  'item:guardian:gear': 'Item (Guardian)',
+  'collectible:guardian:random': 'Collectible (Guardian)',
+  'collectible:historical:random': 'Collectible (Historical)',
+
+  'item:teleportscroll:random': 'Teleport Scroll (Random)',
+  'item:teleportscroll:astralcontrolroom': 'Teleport Scroll (ACR)',
+  'item:buffscroll:random': 'Buff Scroll (Random)'
+}
