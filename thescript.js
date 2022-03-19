7ae0b3c30e3609b8a662b02036b38c893c9b2b25
Marno van der Maas <34654485+marnovandermaas@users.noreply.github.com>
2022-03-19 21:52:20 +0000

    Update todos and changelog

--- thescript.js
+++ thescript.js
@@ -43,13 +43,15 @@
 //  - Auto collect global quests
 //  - (started) Add divine stepper (DivineStumbler variant)
 //  - (done) Auto pet ability usage (request from Anten)
-//  - Implement auto by pots, code provided by Sarimash
+//  - (done) Implement auto by pots, code provided by Sarimash
 //  - (done) Implement reroll quests, code provided by Sarimash
 //  - (done) Add settings tab to control the scalar value at which each type of quest is rerolled
 //
 // Changelog (adapted from changelog.txt):
 //  1.8 (ongoing)
 //    - Added Quest Reroll logic and toggle to enable it
+//    - Added settings to control quest reroll
+//    - Added pot buying logic and toggle to enable it
 //  1.7
 //    - Fixed the guild raids to throttle when failing, add 10 mins to next raid availability
 //        this way it doesn't spam the server and doesn't use up all the gold
