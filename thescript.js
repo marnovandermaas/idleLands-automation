4ef2044b9eb6e748af2e9106583c267f6c5e7afd
Marno van der Maas <34654485+marnovandermaas@users.noreply.github.com>
2022-03-28 18:33:26 +0100

    Add missing parenthesis

--- thescript.js
+++ thescript.js
@@ -1754,7 +1754,7 @@ const RunDivineDirection = () => {
     if (!!discordGlobalCharacter.divineDirection &&
         ((discordGlobalCharacter.divineDirection.x == discordGlobalCharacter.x && discordGlobalCharacter.divineDirection.y == discordGlobalCharacter.y) ||
          (discordGlobalCharacter.divineDirection.x == 1 && discordGlobalCharacter.divineDirection.y == 1))
-    {
+    ) {
       //Disable current divine direction
       setTimeout( () => {unsafeWindow.__emitSocket('character:divinedirection', {x: 1, y: 1}) }, 500);
     }
