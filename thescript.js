352a038f5de921836373731a1aff97ab75e8c56b
Marno van der Maas <34654485+marnovandermaas@users.noreply.github.com>
2022-03-28 16:06:24 +0100

    Emit only first coord for cooldown

--- thescript.js
+++ thescript.js
@@ -1752,13 +1752,24 @@ const RunDivineDirection = () => {
         discordGlobalCharacter.divineDirection.x == discordGlobalCharacter.x && 
         discordGlobalCharacter.divineDirection.y == discordGlobalCharacter.y)
     {
+      let storedDirection = discordGlobalCharacter.divineDirection;
+      //Disable current divine direction
+      setTimeout( () => {unsafeWindow.__emitSocket('character:divinedirection', storedDirection) }, 500);
+      //Find next coordinate in path that is not in cooldown
+      let sentCoord = false;
       for (let i = 0; i < path.length; i++) {
         let cooldowns = Object.keys(discordGlobalCharacter.cooldowns);
         for (let j = 0; j < cooldowns.length; j++) {
           if (cooldowns[j].includes(`${path[i].x},${path[i].y}`) && cooldowns[j] > Date.now()) continue;
         }
         setTimeout( () => {unsafeWindow.__emitSocket('character:divinedirection', path[i]) }, 500);
-      } 
+        sentCoord = true;
+        break;
+      }
+      //If we have not sent out a coordinate reset the DD that was previously there
+      if(!sentCoord) {
+        setTimeout( () => {unsafeWindow.__emitSocket('character:divinedirection', storedDirection) }, 500);
+      }
     }
   } else {
   }
